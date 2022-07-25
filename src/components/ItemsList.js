876f068305d28605cab0ca9841b1684b3335579b
Dima <iamdimadem@gmail.com>
2022-07-25 14:22:20 +0300

    add ItemsList

---
+++ src/components/ItemsList.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Item from "./Item";
+
+export default function ItemsList(props) {
+  const itemsList = props.items.map((item) => (
+    <li className="ui-item-list" key={item.id}>
+      <Item info={item} />
+      <label htmlFor="id-itemButton">Удалить:</label>
+      <button
+        id="id-itemButton"
+        className="item-button"
+        onClick={onHandleDeleteItem(item.id)}>
+        Удалить
+      </button>
+    </li>
+  ));
+  
+  return (
+    <ul className="shop">{itemsList}</ul>
+  )
+};
